145c3f44aca060c30a37425168758874cbc05959
Lakshmi Padmini Kodi <k.padmini@gmail.com>
2023-02-18 09:54:19 -0500

    "Api_loan jupyter and python files"

---
+++ src/api_loan.py
@@ -0,0 +1,33 @@
+import findspark
+findspark.init()
+
+import requests
+import json
+from pprint import pp
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import pandas as pd
+
+spark = SparkSession.builder.master("local[1]").appName('ApiData.com').getOrCreate()
+baseurl = "https://raw.githubusercontent.com/platformps/LoanDataset/main/loan_data.json"
+#endpoint = "planets"
+
+def main_request(baseurl):
+    r = requests.get(baseurl )
+    print(r.status_code)
+    data = r.json()
+    return data
+data = main_request(baseurl)
+#pp(data)
+rdd= spark.sparkContext.parallelize(data)
+df = rdd.toDF()
+df.show()
+df.write.format("jdbc").mode("append")\
+        .option("url","jdbc:mysql://localhost:3306/creditcard_capstone")\
+        .option("dbtable","creditcard_capstone.CDW_SAPP_loan_application")\
+        .option("user","root")\
+        .option("password","password")\
+        .save()
