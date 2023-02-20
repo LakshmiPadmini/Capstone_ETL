af4735480d90d102c3ff9161291432dd23b9772d
Lakshmi Padmini Kodi <k.padmini@gmail.com>
2023-02-19 23:31:12 -0500

    Apllication loan dataset visulization jupyter file

--- src/api_plots.py
+++ src/api_plots.py
@@ -2,6 +2,7 @@ import pymysql
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas.io.sql as psql
+import seaborn as sns
 
 connection = pymysql.connect(
     host='localhost',
@@ -16,7 +17,22 @@ cursor.execute(sql)
 result_set = cursor.fetchall()
 data_frame = psql.read_sql(sql, con=connection)
 pd.set_option("display.max_columns", None)
-print(data_frame.head())
+#print(data_frame.head())
+
 
 cursor.close()
 connection.close()
+#print(data_frame)
+
+#print(data)
+
+data = len(data_frame)
+data1 = data_frame.groupby(by = 'Self_Employed')["Application_Status"].value_counts()
+
+
+self_emp_approved = data_frame.loc[(data_frame['Self_Employed'] == 'Yes') & (data_frame['Application_Status'] == 'Y'), 'Application_Status'].count()
+print(self_emp_approved)
+percentage = self_emp_approved/data*100
+print(percentage)
+sns_plot = sns.barplot(x="Self_Employed", y="Application_Status", data=data1, estimator=percentage)
+sns_plot.barh
